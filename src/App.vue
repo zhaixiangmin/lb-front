ceab18df4a2af89ff0f7351c16358083aa9c974c
licong <262832930@qq.com>
2021-05-13 12:09:16 +0800

    初始化工程

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="app">
+<!--    <div id="nav">-->
+<!--      <router-link to="/">Home</router-link> |-->
+<!--      <router-link to="/about">About</router-link>-->
+<!--    </div>-->
+<!--    登录页面-->
+    <router-view v-if="$route.path === '/login'" />
+<!--    非登录页面，采用统一布局-->
+    <Layout v-else>
+      <router-view/>
+    </Layout>
+  </div>
+</template>
+
+<script>
+import Layout from "./components/Layout";
+export default {
+  name: 'App',
+  components: {
+    Layout
+  },
+  created() {
+    console.log('$route', this.$route)
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+//#nav {
+//  padding: 30px;
+//
+//  a {
+//    font-weight: bold;
+//    color: #2c3e50;
+//
+//    &.router-link-exact-active {
+//      color: #42b983;
+//    }
+//  }
+//}
+</style>
---
+++ src/components/Layout.vue
@@ -0,0 +1,64 @@
+<template>
+  <el-container>
+    <el-header height="220px">
+      <div class="title-container">
+        <div class="title-box">
+          <div class="title">财务记账服务模板</div>
+          <el-button type="text" icon="el-icon-phone">000-0000000</el-button>
+        </div>
+      </div>
+    </el-header>
+    <el-main>
+      主要区域容器
+      <slot></slot>
+    </el-main>
+    <el-footer>
+      底栏容器
+    </el-footer>
+  </el-container>
+</template>
+
+<script>
+export default {
+  name: "Layout"
+}
+</script>
+
+<style scoped lang="scss">
+.el-container {
+  .el-header {
+    padding: 0;
+
+    .title-container {
+      height: 120px;
+      line-height: 120px;
+      background: #F13A3A;
+
+      .title-box {
+        margin: 0 auto;
+        width: 960px;
+        display: flex;
+        justify-content: space-between;
+        color: #fff;
+
+        .title {
+          font-size: 32px;
+          font-weight: bold;
+        }
+
+        .el-button ::v-deep {
+          display: flex;
+          align-items: center;
+          font-size: 24px;
+          font-weight: bold;
+          color: #fff;
+
+          i.el-icon-phone {
+            font-size: 42px;
+          }
+        }
+      }
+    }
+  }
+}
+</style>
